5773ed03cc6e430f5d804872598b92a8d8a873da
heewonkim-dev <heewon_kim@outlook.com>
2021-11-12 17:57:15 +0900

    CHORE : 기본 구조 업데이트

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home/index";
+import Mypage from "./routes/Mypage/index";
+import Resume from "./routes/Resume/index";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/mypage" element={<Mypage />} />
+          <Route exact path="/resume" element={<Resume />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ client/src/routes/Resume/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Index() {
+  return <div>Resume</div>;
+}
+
+export default Index;
