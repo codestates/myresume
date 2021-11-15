b110dd74d302fb4a2b6084700899673f4a6f8157
taesubyun <taesubyun@icloud.com>
2021-11-15 18:23:20 +0900

    FEAT : Resume 페이지 와이어프레임

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./routes/Home/index";
-import Mypage from "./routes/Mypage/index";
-import Resume from "./routes/Resume/index";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home/index';
+import Mypage from './routes/Mypage/index';
+import Resume from './routes/Resume/index';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/mypage" element={<Mypage />} />
-          <Route exact path="/resume" element={<Resume />} />
+          <Route exact path='/home' element={<Home />} />
+          <Route exact path='/mypage' element={<Mypage />} />
+          <Route exact path='/' element={<Resume />} />
         </Routes>
       </Router>
     </div>
--- client/src/components/SignupLogin.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function SignupLogin() {
-  return <div>Modal</div>;
-}
-
-export default SignupLogin;
