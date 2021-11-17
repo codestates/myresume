0b4f6edb12cf3907f031c7dfe393f7a893193210
Taesub Yun <taesubyun@icloud.com>
2021-11-17 17:32:34 +0900

    FEAT : 레이아웃 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./routes/Home/index";
-import Mypage from "./routes/Mypage/index";
-import Resume from "./routes/Resume/index";
-import Navbar from "./components/navbar/Navbar";
-import Footer from "../src/routes/Home/Sections/Footer";
-import Signup from "../src/routes/Home/pages/Signup";
-import Story from "routes/Story";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home/index';
+import Mypage from './routes/Mypage/index';
+import Resume from './routes/Resume/index';
+import Navbar from './components/navbar/Navbar';
+import Footer from '../src/routes/Home/Sections/Footer';
+import Signup from '../src/routes/Home/pages/Signup';
+import Story from 'routes/Story';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/mypage" element={<Mypage />} />
-          <Route path="/resume" element={<Resume />} />
-          <Route path="/sign-up" element={<Signup />} />
-          <Route path="story/*" element={<Story />} />
+          <Route exact path='/' element={<Home />} />
+          <Route path='/mypage' element={<Mypage />} />
+          <Route path='/resume' element={<Resume />} />
+          <Route path='/sign-up' element={<Signup />} />
+          <Route path='story/*' element={<Story />} />
         </Routes>
         <Footer />
       </Router>
